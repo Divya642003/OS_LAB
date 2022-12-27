25d996cbb5843d9406f5e796d5b471690217c417
Divya Singh <divyasingh.300246@gmail.com>
2022-12-27 18:13:42 +0530

    more advance fcfs code

--- FCFS.c
+++ FCFS.c
@@ -1,70 +1,71 @@
 #include<stdio.h>
 
-void waiting_time(int processes[],int n,int bt[],int wt[])
-{
-    
-    wt[0]=0;
-    for(int i=1;i<n;i++)
-    {
-        wt[i] = bt[i-1]+wt[i-1];
-    }
-    for(int i=0;i<n;i++)
-    {
-        printf("%d\t\t\t %d\n",processes[i],wt[i]);
-    }
-  
-}
+typedef struct process{
+    int p_id;
+    int arrival_time;
+    int burst_time;
+    int comp_time;
+    int waiting_time;
+    int turnaround_time;
+}process;
 
-void Turnaround_time(int processes[],int n,int bt[],int wt[],int tat[])
-{
-    
-    for(int i=0;i<n;i++)
-    {
-        tat[i] = bt[i]+wt[i];
-    }
-    for(int i=0;i<n;i++)
-    {
-        printf("%d\t\t\t %d\n",processes[i],tat[i]);
-    }
-  
-}
-
-void average_time(int processes[],int n,int bt[],int wt[],int tat[])
-{
-  int total_waiting_time=0;
-  int total_turnaround_time=0;
-    for(int i=0;i<n;i++)
-    {
-        total_waiting_time = total_waiting_time+wt[i];
-        total_turnaround_time = total_turnaround_time+tat[i];
-    }
-    printf("%d\t\t\t %d\n",total_waiting_time/n,total_turnaround_time/n);
-}
 int main()
 {
-    
     int n;
-    
-    printf("\n Enter the number of processes :: ");
+    printf("\n ENTER THE TOTAL NUMBER OF PROCESSES :: ");
     scanf("%d",&n);
-    int processes[n];
-    int bt[n];
-    int wt[n];
-    int tat[n];
-    printf("_________________________________________________________________________\n");
-    printf(" ENTER THE PROCESSES :    BURST TIME :    \n");
-    for(int i=0;i<n;i++)
+    process array[n];
+    int i;
+    printf("\nENTER                            P_ID    ARRIVAL_TIME    BURST_TIME  ");
+    for(i=0;i<n;i++)
     {
-        scanf("%d%d",&processes[i],&bt[i]);
+        printf("\nENTER RECORD FOR PROCESS %d ",i+1);
+        scanf("%3d %5d %7d",&array[i].p_id,&array[i].arrival_time,&array[i].burst_time);
+
+        if(i>0)
+        {
+         process key=array[i];
+            int j=i-1;
+            while(j>=0&&key.arrival_time<array[j].arrival_time)
+            {
+                array[j+1]=array[j];
+                j=j-1;
+            }
+            array[j+1]=key;
+        }
     }
-    //printf("Hello");
-    printf("_________________________________________________________________________\n");
-    printf("THE WAITING TIME FOR %d PROCEESE IS :: \n",n);
-    waiting_time(processes,n,bt,wt);
-    printf("_________________________________________________________________________\n");
-    printf("THE TURN_AROUND TIME FOR %d PROCEESE IS :: \n",n);
-    Turnaround_time(processes,n,bt,wt,tat);
-     printf("_________________________________________________________________________\n");
-    printf("THE AVERAGE WAITING TIME AND AVERAGE TURN_AROUND TIME FOR %d PROCEESE IS :: \n",n);
-    average_time(processes,n,bt,wt,tat);
- }
+
+   /* for(i=0;i<n;i++)
+    {
+        printf("\n%d",array[i].p_id);
+    }*/
+
+    int idle_time=0;
+    int time=array[0].arrival_time;
+    for(i=0;i<n;i++)
+    {
+        if(array[i].arrival_time>time)
+        {
+            idle_time++;
+            time++;
+            i--;
+            continue;
+        }
+        array[i].comp_time=time+array[i].burst_time;
+        array[i].turnaround_time=array[i].comp_time-array[i].arrival_time;
+        array[i].waiting_time=array[i].turnaround_time-array[i].burst_time;
+                  
+        time+=array[i].burst_time;
+    }
+    float avg_wait_time =0;
+    float cpu_utilization =((time-idle_time)/(float)time)*100;
+ printf("\n  P_ID    ARRIVAL_TIME    BURST_TIME    COMP_TIME   TURNAROUND_TIME   WAITING_TIME ");
+for(i=0;i<n;i++)
+    {
+        printf("\n%3d %7d %15d %15d %15d %15d",array[i].p_id,array[i].arrival_time,array[i].burst_time,array[i].comp_time,array[i].turnaround_time,array[i].waiting_time);
+        avg_wait_time+=array[i].waiting_time;
+    }   
+
+    printf("\n TOTAL AVERAGE WAITING TIME IS :: %f",avg_wait_time/n);
+    printf("\n TOTAL CPU UTILIZATION IN percent IS :: %f",cpu_utilization ); 
+}
